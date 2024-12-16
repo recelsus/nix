ec822bb5ae35d4a3e6ea855a16f3e559ec005d09
recelsus <rhinfunc+git@gmail.com>
2024-12-16 17:32:03 +0900

    Refactor: Configure flake to use flake config
    This commit refactors the flake to accept a configuration object, allowing for greater flexibility and maintainability.  It moves environment-specific settings from direct imports of `/etc/nixos` files to a configuration object passed into the flake's `outputs`.
    
    - flake.nix
        - Removed direct imports of `/etc/nixos/hardware-configuration.nix` and `/etc/nixos/env.nix`.
        - Added a `config` argument to the `outputs` function.
        - Used the `config` object to access hardware configuration, user configuration, and network settings.
        - Updated user configuration to use the `config.env.type` and `config.env.user` values.
        - Modified network settings to use values from the `config.env` object.
        - Adjusted boot device setting to use `config.env.bootDevice`.

--- flake.nix
+++ flake.nix
@@ -9,39 +9,37 @@
     };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: { config, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          # ローカルのenv.nixとhardware-configuration.nixを直接読み込む
-          (import /etc/nixos/hardware-configuration.nix)
+          config.hardwareConfig
 
           home-manager.nixosModules.home-manager {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               users = {
-                # env.nixの値を利用
-                ${(import /etc/nixos/env.nix).user or "default-user"} = import ./types/${(import /etc/nixos/env.nix).type or "default"}.nix;
+                ${config.env.user or "default-user"} = import ./types/"${config.env.type or "default"}.nix";
               };
             };
           }
 
           {
             boot.loader.grub.enable = true;
-            boot.loader.grub.devices = [ (import /etc/nixos/env.nix).bootDevice or "/dev/sda" ];
+            boot.loader.grub.devices = [ config.env.bootDevice or "/dev/sda" ];
 
             networking = {
-              hostName = (import /etc/nixos/env.nix).hostname or "nixos";
-              defaultGateway = (import /etc/nixos/env.nix).gateway or "192.168.1.1";
+              hostName = config.env.hostname or "nixos";
+              defaultGateway = config.env.gateway or "192.168.1.1";
               interfaces.eth0.ipv4.addresses = [ {
-                address = (import /etc/nixos/env.nix).ipv4Address or "192.168.1.100";
+                address = config.env.ipv4Address or "192.168.1.100";
                 prefixLength = 24;
               } ];
-              nameservers = (import /etc/nixos/env.nix).nameservers or [ "8.8.8.8" ];
-              firewall.enable = (import /etc/nixos/env.nix).firewall or false;
-              firewall.allowedTCPPorts = (import /etc/nixos/env.nix).allowedPorts or [];
+              nameservers = config.env.nameservers or [ "8.8.8.8" ];
+              firewall.enable = config.env.firewall or false;
+              firewall.allowedTCPPorts = config.env.allowedPorts or [];
             };
 
             services.openssh.enable = true;
