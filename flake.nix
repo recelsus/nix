b8442ede06c543e37f4b35d3470edd318c3c7eed
recelsus <rhinfunc+git@gmail.com>
2024-12-16 18:23:45 +0900

    Refactor: Directly import configuration files
    - Removed `config-env` and `config-hardware` inputs.
    - Directly import `/etc/nixos/hardware-configuration.nix` and `/etc/nixos/env.nix`.
    
    - flake.nix
      - Removed `config-env` and `config-hardware` from inputs.
      - Replaced references to `inputs.config-env.url` and `inputs.config-hardware.url` with direct imports of `/etc/nixos/env.nix` and `/etc/nixos/hardware-configuration.nix` respectively.

--- flake.nix
+++ flake.nix
@@ -2,28 +2,19 @@
   description = "NixOS Configuration with Home Manager (External)";
 
   inputs = {
-    # NixpkgsとHome Manager
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # ローカルファイルを参照
-    config-env = {
-      url = "path:/etc/nixos/env.nix";
-    };
-    config-hardware = {
-      url = "path:/etc/nixos/hardware-configuration.nix";
-    };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, config-env, config-hardware, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          (import inputs.config-hardware.url) # ローカルのhardware-configuration.nix
+          (import /etc/nixos/hardware-configuration.nix) # ローカルのhardware-configuration.nixを直接参照
 
           home-manager.nixosModules.home-manager {
             home-manager = {
@@ -31,8 +22,8 @@
               useUserPackages = true;
               users = {
                 # ローカルのenv.nixから動的にユーザー名を取得
-                ${(import inputs.config-env.url).user or "default-user"} =
-                  import ./types/${(import inputs.config-env.url).type or "default"}.nix;
+                ${(import /etc/nixos/env.nix).user or "default-user"} =
+                  import ./types/"${(import /etc/nixos/env.nix).type or "default"}.nix";
               };
             };
           }
@@ -40,27 +31,27 @@
           {
             # ブートローダー設定
             boot.loader.grub.enable = true;
-            boot.loader.grub.devices = [ (import inputs.config-env.url).bootDevice or "/dev/sda" ];
+            boot.loader.grub.devices = [ (import /etc/nixos/env.nix).bootDevice or "/dev/sda" ];
 
             # ネットワーク設定
             networking = {
-              hostName = (import inputs.config-env.url).hostname or "nixos";
-              defaultGateway = (import inputs.config-env.url).gateway or "192.168.1.1";
+              hostName = (import /etc/nixos/env.nix).hostname or "nixos";
+              defaultGateway = (import /etc/nixos/env.nix).gateway or "192.168.1.1";
               interfaces.eth0.ipv4.addresses = [ {
-                address = (import inputs.config-env.url).ipv4Address or "192.168.1.100";
+                address = (import /etc/nixos/env.nix).ipv4Address or "192.168.1.100";
                 prefixLength = 24;
               } ];
-              nameservers = (import inputs.config-env.url).nameservers or [ "8.8.8.8" ];
-              firewall.enable = (import inputs.config-env.url).firewall or false;
-              firewall.allowedTCPPorts = (import inputs.config-env.url).allowedPorts or [];
+              nameservers = (import /etc/nixos/env.nix).nameservers or [ "8.8.8.8" ];
+              firewall.enable = (import /etc/nixos/env.nix).firewall or false;
+              firewall.allowedTCPPorts = (import /etc/nixos/env.nix).allowedPorts or [];
             };
 
             # ユーザー設定
-            users.users."${(import inputs.config-env.url).user or "default-user"}" = {
+            users.users."${(import /etc/nixos/env.nix).user or "default-user"}" = {
               isNormalUser = true;
-              extraGroups = (import inputs.config-env.url).groups or [];
-              initialPassword = (import inputs.config-env.url).password or "default-password";
-              openssh.authorizedKeys.keys = (import inputs.config-env.url).authorizedKeys or [];
+              extraGroups = (import /etc/nixos/env.nix).groups or [];
+              initialPassword = (import /etc/nixos/env.nix).password or "default-password";
+              openssh.authorizedKeys.keys = (import /etc/nixos/env.nix).authorizedKeys or [];
             };
 
             # Sudo設定
@@ -78,11 +69,8 @@
             virtualisation.docker.enable = true;
 
             # GUI設定
-            imports = if (import inputs.config-env.url).gui then [ ./gui-config.nix ] else [];
+            imports = if (import /etc/nixos/env.nix).gui then [ ./gui-config.nix ] else [];
           }
         ];
       };
-    };
-  };
-}
 
