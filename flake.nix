73aa87fb9df1f3ce5dec1aedbb3091b0c13ecee3
recelsus <rhinfunc+git@gmail.com>
2024-12-16 18:15:30 +0900

    Refactor: Externalize NixOS configuration via separate module imports
    - Moved hardware configuration to `/etc/nixos/hardware-configuration.nix`.
    - Moved environment variables to `/etc/nixos/env.nix`.
    - Refactored flake.nix to import these configurations.
    
    - flake.nix
      - Imports hardware and environment configurations.
      - Replaced inline config values with imported values.
      - Added network, user, sudo, SSH and Docker configurations.
      - Conditional GUI config import based on env variable.

--- flake.nix
+++ flake.nix
@@ -7,49 +7,71 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    config-env.url = "path:/etc/nixos/env.nix";
+    config-hardware.url = "path:/etc/nixos/hardware-configuration.nix";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: { config, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, config-env, config-hardware, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          # `config.hardwareConfig` を参照
-          config.hardwareConfig
+          (import config-hardware.path)
 
-          # Home Manager の設定
           home-manager.nixosModules.home-manager {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
               users = {
-                ${config.env.user or "default-user"} = import ./types/"${config.env.type or "default"}.nix";
+                ${(import config-env.path).user or "default"} =
+                  import ./types/"${(import config-env.path).type or "default"}.nix";
               };
             };
           }
 
-          # その他の設定
           {
+            # Boot Loader Config
             boot.loader.grub.enable = true;
-            boot.loader.grub.devices = [ config.env.bootDevice or "/dev/sda" ];
+            boot.loader.grub.devices = [ (import config-env.path).bootDevice or "/dev/sda" ];
 
+            # Network Config
             networking = {
-              hostName = config.env.hostname or "nixos";
-              defaultGateway = config.env.gateway or "192.168.1.1";
+              hostName = (import config-env.path).hostname or "nixos";
+              defaultGateway = (import config-env.path).gateway or "192.168.1.1";
               interfaces.eth0.ipv4.addresses = [ {
-                address = config.env.ipv4Address or "192.168.1.100";
+                address = (import config-env.path).ipv4Address or "192.168.1.100";
                 prefixLength = 24;
               } ];
-              nameservers = config.env.nameservers or [ "8.8.8.8" ];
-              firewall.enable = config.env.firewall or false;
-              firewall.allowedTCPPorts = config.env.allowedPorts or [];
+              nameservers = (import config-env.path).nameservers or [ "8.8.8.8" ];
+              firewall.enable = (import config-env.path).firewall or false;
+              firewall.allowedTCPPorts = (import config-env.path).allowedPorts or [];
             };
 
+            # User Config
+            users.users."${(import config-env.path).user or "default-user"}" = {
+              isNormalUser = true;
+              extraGroups = (import config-env.path).groups or [];
+              initialPassword = (import config-env.path).password or "default-password";
+              openssh.authorizedKeys.keys = (import config-env.path).authorizedKeys or [];
+            };
+
+            # sudo Config
+            security.sudo.extraRules = [
+              {
+                groups = ["wheel"];
+                commands = [ { command = "ALL"; options = [ "NOPASSWD" ]; } ];
+              }
+            ];
+
+            # SSH Config
             services.openssh.enable = true;
+
+            # Docker Config
             virtualisation.docker.enable = true;
 
-            # GUI 設定
-            imports = if config.env.gui then [ ./gui-config.nix ] else [];
+            # GUI Config
+            imports = if (import config-env.path).gui then [ ./gui-config.nix ] else [];
           }
         ];
       };
